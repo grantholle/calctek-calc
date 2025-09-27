79b769a6b4249e984568dc0f8e765685216a4dde
Grant Holle <hollegrant@gmail.com>
2025-09-27 16:54:02 -0500

    Add message to shared props

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -38,6 +38,7 @@ class HandleInertiaRequests extends Middleware
     {
         return [
             ...parent::share($request),
+            'message' => fn () => session('success'),
         ];
     }
 }
